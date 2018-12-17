fb406181f8b53e320e2c31efb75f5ebd122afe4b
Epinder Singh <epinder.singh@gmail.com>
2018-12-17 16:38:23 +0800

    Section 7 Chapter 15 done

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hello')
+const paragraphs = document.querySelectorAll('p')
+
+paragraphs.forEach(function (p) {
+    if(p.textContent.includes('the')) {
+        p.remove()
+    }
+})
+
